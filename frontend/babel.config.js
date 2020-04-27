079a03ad2b1bb455a170745a9c06dfee2aba067c
liuyuxian <yuxian.liu@chaitin.com>
2020-04-27 17:03:55 +0800

    babel config

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ["@babel/preset-env"],
-};
+  presets: [
+    '@babel/preset-env',
+    {
+      targets: {
+        browsers: ['>0.1%', 'ie >= 10', 'Chrome >= 32', 'Firefox >= 31'],
+      },
+      useBuiltIns: 'usage',
+    },
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+}
